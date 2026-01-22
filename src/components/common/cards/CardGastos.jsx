1fb000386a1be3d0addd687dce886f0b675690d9
vbchan <vbchan@frd.org.mx>
2026-01-22 13:54:40 -0600

    card de las gastos semi listos

---
+++ src/components/common/cards/CardGastos.jsx
@@ -0,0 +1,11 @@
+export function CardGastos() {
+    return (
+        <section className="border p-3 min-w-[250px] rounded-lg">
+            <p className="text-gray-800 text-sm">Gastos de hoy</p>
+            <div>
+                <p className="text-3xl font-bold mt-3">$0.00</p>
+                <span className="text-gray-800 text-sm">0 gastos registrados</span>
+            </div>
+        </section>
+    );
+}
--- src/pages/Gastos.jsx
+++ src/pages/Gastos.jsx
@@ -1,7 +1,14 @@
+import { CardGastos } from "../components/common/cards/CardGastos";
+
 function Gastos(){
     return (
         <div>
-            <h1>Gastos</h1>
+            <div className="flex items-center justify-between">
+                <CardGastos />
+                <CardGastos />
+                <CardGastos />
+                <CardGastos />
+            </div>
         </div>
     )
 }
